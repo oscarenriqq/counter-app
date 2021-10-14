482c8a2c6c49b33c855c41ac1e3756995490be79
Oscar Molinar <oscarmolinar96@gmail.com>
2021-10-14 15:37:51 -0500

    Testing VSCODE

--- src/Counter.js
+++ src/Counter.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Counter = () => {
     return (
         <div>
-            <h1> Counter App JS </h1>
+            <h1> Counter App </h1>
         </div>
     )
 }
